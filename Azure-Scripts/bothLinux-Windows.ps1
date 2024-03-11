35ceac05bc49b5f6dfbf510a13b095a4791625f8
cchantoftp <157639533+cchantoftp@users.noreply.github.com>
2024-03-11 08:29:02 -0600

    Update Linux-Windows.ps1
    New script that runs over all the RG, Note= user should have access, otherwise the script will ignore it.

--- Azure-Scripts/bothLinux-Windows.ps1
+++ Azure-Scripts/bothLinux-Windows.ps1
@@ -1,18 +1,18 @@
 #az login
 
 # Set your Azure subscription
-#$subscriptionId = "b34abaea-961b-4fc8-8e8b-38118036e83b"
+#$subscriptionId = "Your subcription ID here"
 #az account set --subscription $subscriptionId
 
 # Variables
-$resourceGroupName = "nf-test"
 $outputCsv = "1vm_disk_inventory.csv"
 
 # Function to get disk info from VM's guest OS
 function Get-GuestOSDiskInfo {
     param (
         $vmName,
-        $osType
+        $osType,
+        $resourceGroupName
     )
 
     if ($osType -eq "Linux") {
@@ -23,45 +23,58 @@ function Get-GuestOSDiskInfo {
     return $diskInfo.Trim()
 }
 
-# Getting VM details
-$vms = az vm list -g $resourceGroupName --query "[].{Name:name, OsType:storageProfile.osDisk.osType}" | ConvertFrom-Json
+# Get all resource groups in the subscription
+$resourceGroups = az group list --query "[].name" -o tsv
 
 # Array to hold all data
 $allData = @()
 
-foreach ($vm in $vms) {
-    $vmName = $vm.Name
-    $osType = $vm.OsType
+foreach ($resourceGroupName in $resourceGroups) {
+    Write-Host "Checking VMs in resource group: $resourceGroupName"
+    # Getting VM details in each resource group
+    $vms = az vm list -g $resourceGroupName --query "[].{Name:name, OsType:storageProfile.osDisk.osType}" | ConvertFrom-Json
 
-    # Get Azure Disk Details
-    # Fetching the ID of the OS disk and data disks from the VM object
-    $osDiskId = az vm show -g $resourceGroupName -n $vmName --query "storageProfile.osDisk.managedDisk.id" -o tsv
-    $dataDisksIds = az vm show -g $resourceGroupName -n $vmName --query "storageProfile.dataDisks[].managedDisk.id" -o tsv
+    foreach ($vm in $vms) {
+        $vmName = $vm.Name
+        $osType = $vm.OsType
 
-    # Combining OS and data disks IDs into an array
-    $diskIds = @($osDiskId) + @($dataDisksIds)
+        Write-Host "Checking VM: $vmName"
 
-    # Get Guest OS Disk Details
-    $osDiskInfo = Get-GuestOSDiskInfo -vmName $vmName -osType $osType
+        try {
+            # Get Azure Disk Details
+            # Fetching the ID of the OS disk and data disks from the VM object
+            $osDiskId = az vm show -g $resourceGroupName -n $vmName --query "storageProfile.osDisk.managedDisk.id" -o tsv
+            $dataDisksIds = az vm show -g $resourceGroupName -n $vmName --query "storageProfile.dataDisks[].managedDisk.id" -o tsv
 
-    foreach ($diskId in $diskIds) {
-        # Get details for each disk using its ID
-        if (-not [string]::IsNullOrWhiteSpace($diskId)) {
-            $diskDetails = az disk show --ids $diskId --query "{Name:name, DiskSizeGB:diskSizeGb, DiskType:sku.name}" | ConvertFrom-Json
+            # Combining OS and data disks IDs into an array
+            $diskIds = @($osDiskId) + @($dataDisksIds)
 
-            # Construct the data object for each disk
-            $dataObj = New-Object PSObject -Property @{
-                VMName             = $vmName
-                DiskType           = if ($osDiskId -eq $diskId) { "OS Disk" } else { "Data Disk" }
-                DiskName           = $diskDetails.Name
-                AzureDiskSizeGB    = $diskDetails.DiskSizeGB
-                AzureDiskType      = $diskDetails.DiskType
-                AzureDiskId        = $diskId
-                OSDiskInfo         = if ($osDiskId -eq $diskId) { $osDiskInfo } else { "" }
+            # Get Guest OS Disk Details
+            $osDiskInfo = Get-GuestOSDiskInfo -vmName $vmName -osType $osType -resourceGroupName $resourceGroupName
+
+            foreach ($diskId in $diskIds) {
+                # Get details for each disk using its ID
+                if (-not [string]::IsNullOrWhiteSpace($diskId)) {
+                    $diskDetails = az disk show --ids $diskId --query "{Name:name, DiskSizeGB:diskSizeGb, DiskType:sku.name}" | ConvertFrom-Json
+
+                    # Construct the data object for each disk
+                    $dataObj = New-Object PSObject -Property @{
+                        ResourceGroupName = $resourceGroupName
+                        VMName            = $vmName
+                        DiskType          = if ($osDiskId -eq $diskId) { "OS Disk" } else { "Data Disk" }
+                        DiskName          = $diskDetails.Name
+                        AzureDiskSizeGB   = $diskDetails.DiskSizeGB
+                        AzureDiskType     = $diskDetails.DiskType
+                        AzureDiskId       = $diskId
+                        OSDiskInfo        = if ($osDiskId -eq $diskId) { $osDiskInfo } else { "" }
+                    }
+
+                    # Add the data object to the array of all data
+                    $allData += $dataObj
+                }
             }
-
-            # Add the data object to the array of all data
-            $allData += $dataObj
+        } catch {
+            Write-Host "Skipping VM '$vmName' in resource group '$resourceGroupName' as it is not running or encountered an error."
         }
     }
 }
